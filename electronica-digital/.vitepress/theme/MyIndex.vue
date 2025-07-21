ab9c670329379048388517ab0f8e3838493b96a1
Francesc <francesc.madrid@upc.edu>
2025-07-21 13:30:50 +0200

    missatge

---
+++ electronica-digital/.vitepress/theme/MyIndex.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div v-for="item of index">
+            <h3>{{ item.text }}</h3>
+            <ul>
+                <div v-for="subitem of item.items">
+                    <li>
+                        <a :href="'/electronica-digital' + subitem.link">
+                            <div v-html="subitem.text"></div>
+                        </a>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { index } from '../index'
+import { ref, onMounted } from 'vue'
+</script>
+
+<style scoped>
+a:link {
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+</style>
---
+++ electronica-digital/src/index.md
@@ -0,0 +1,17 @@
+---
+layout: home
+
+hero:
+    name: 'Lliçons Template'
+    tagline: 'Jordi Petit <small><i>et al</i></small>'
+    no-text: lliçons.jutge.org/template
+
+    actions:
+        - theme: brand
+          text: Inici
+          link: /inici/
+---
+
+<h1 class="tagline">Taula de continguts</h1>
+
+<MyIndex/>
